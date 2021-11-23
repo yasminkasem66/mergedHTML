ef49da801d6d99bfc5d0e017c06fa5f27ef75ff5
yasmin <yasmin.abdellh@gmail.com>
2021-11-23 21:14:27 +0200

    merged

--- js/index.js
+++ js/index.js
@@ -14,3 +14,22 @@ window.addEventListener("scroll", function () {
     // }
 });
 
+
+//single product element with
+var element = document.getElementById("left");
+
+window.addEventListener("scroll", function () {
+  var height = window.scrollY;
+  if (height >= 0) {
+    element.style.position = "sticky";
+    element.style.top = 0;
+    element.style.left = 0;
+    // element.style.width = "100%";
+  }
+  // else {
+  //     //  element.style.marginTop = 150 + "px";
+
+  // }
+});
+
+
